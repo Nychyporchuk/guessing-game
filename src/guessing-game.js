cc8aafef85c1470d15a5209dedd2c32a61ce837f
Nychyporchuk <bloodever@gmail.com>
2024-10-24 14:54:43 +0300

    fix: did it

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,29 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.minVal = null;
+        this.maxVal = null;
+        this.currentGuess = null;
+    }
 
     setRange(min, max) {
-
+        this.minVal = min;
+        this.maxVal = max;
     }
 
     guess() {
 
+        this.currentGuess = Math.ceil((this.minVal + this.maxVal) / 2);
+        return this.currentGuess;
     }
 
     lower() {
 
+        this.maxVal = this.currentGuess;
     }
 
     greater() {
 
+        this.minVal = this.currentGuess;
     }
 }
 
